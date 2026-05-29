b37437667d91d9af6708af22a66dc8efe7dd57ba
Andrea <denissedepaz16@gmail.com>
2026-05-28 19:20:45 -0600

    Andrea Denisse Ortiz De Paz-Ejercicio2- pagina 61- finalizado

--- src/poo/pagina61/ejercicio2/Main.java
+++ src/poo/pagina61/ejercicio2/Main.java
@@ -1,4 +1,20 @@
 package poo.pagina61.ejercicio2;
 
-public class Main {
-}
+import java.util. Scanner;
+       public class Main {
+
+      public static void main(String[] args){
+
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Ingrese Texto");
+
+          String texto = sc.nextLine();
+
+          UtilCadena util = new UtilCadena();
+          String resultado = util.QuitarEspacios(texto);
+          
+          System.out.println("Resultado:"+ resultado);
+
+
+      }
+       }

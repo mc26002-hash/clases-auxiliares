ac37290c4c016b0916fdea38ef8e183598b0592b
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-29 07:31:05 -0600

    Jose Angel Mate Carias: Ejercicio 6
    Ejercio Finalizado

--- src/poo/pagina61/ejercicio6/Main.java
+++ src/poo/pagina61/ejercicio6/Main.java
@@ -1,4 +1,31 @@
 package poo.pagina61.ejercicio6;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== PALÍNDROMO ===");
+
+        System.out.print("Ingrese una palabra: ");
+        String palabra = scanner.nextLine();
+
+        UtilCadena util = new UtilCadena();
+
+        boolean resultado = util.esPalindromo(palabra);
+
+        if (resultado) {
+
+            System.out.println("La palabra es palíndroma.");
+
+        } else {
+
+            System.out.println("La palabra no es palíndroma.");
+        }
+
+        scanner.close();
+    }
 }
--- src/poo/pagina61/ejercicio6/UtilCadena.java
+++ src/poo/pagina61/ejercicio6/UtilCadena.java
@@ -1,4 +1,18 @@
 package poo.pagina61.ejercicio6;
 
 public class UtilCadena {
+
+    public boolean esPalindromo(String palabra) {
+
+        palabra = palabra.toLowerCase();
+
+        String invertida = "";
+
+        for (int i = palabra.length() - 1; i >= 0; i--) {
+
+            invertida += palabra.charAt(i);
+        }
+
+        return palabra.equals(invertida);
+    }
 }

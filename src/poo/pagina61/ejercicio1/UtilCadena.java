60754201cce05e363f334a1ff73d59e9e2c8769b
Andrea <denissedepaz16@gmail.com>
2026-05-28 18:32:15 -0600

    Andrea Denisse Ortiz De paz- ejercicio 1 -Finalizado -pagina 61

--- src/poo/pagina61/ejercicio1/UtilCadena.java
+++ src/poo/pagina61/ejercicio1/UtilCadena.java
@@ -1,4 +1,10 @@
 package poo.pagina61.ejercicio1;
 
 public class UtilCadena {
+    public int contarpalabras(String frase) {
+
+        String[] palabras= frase.split("");
+
+        return palabras.length;
+    }
 }

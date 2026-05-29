77f03ff950d0fd08be9cab8f615ced7c5f70b6cc
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-29 07:22:04 -0600

    Jose Angel Mate Carias: Ejercicio 5
    Ejercicio Finalizado

--- src/poo/pagina61/ejercicio5/Main.java
+++ src/poo/pagina61/ejercicio5/Main.java
@@ -1,4 +1,24 @@
 package poo.pagina61.ejercicio5;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== CONTAR VOCAL A ===");
+
+        System.out.print("Ingrese una frase: ");
+        String frase = scanner.nextLine();
+
+        UtilCadena util = new UtilCadena();
+
+        int cantidad = util.contarVocalA(frase);
+
+        System.out.println("La vocal A aparece " + cantidad + " veces.");
+
+        scanner.close();
+    }
 }
--- src/poo/pagina61/ejercicio5/UtilCadena.java
+++ src/poo/pagina61/ejercicio5/UtilCadena.java
@@ -1,4 +1,21 @@
 package poo.pagina61.ejercicio5;
 
 public class UtilCadena {
+
+    public int contarVocalA(String frase) {
+
+        int contador = 0;
+
+        frase = frase.toUpperCase();
+
+        for (int i = 0; i < frase.length(); i++) {
+
+            if (frase.charAt(i) == 'A') {
+
+                contador++;
+            }
+        }
+
+        return contador;
+    }
 }

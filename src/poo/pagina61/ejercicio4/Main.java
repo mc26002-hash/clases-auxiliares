4b7adf617f3fce1e47b97acc60ac73fb500af987
mc26002-hash <mc26002@esfe.agape.edu.sv>
2026-05-29 07:03:57 -0600

    Ejercicio 4
    Ejercicio Finalizado

--- src/poo/pagina61/ejercicio4/Main.java
+++ src/poo/pagina61/ejercicio4/Main.java
@@ -1,4 +1,24 @@
 package poo.pagina61.ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== ELIMINAR ÚLTIMA PALABRA ===");
+
+        System.out.print("Ingrese una frase: ");
+        String frase = scanner.nextLine();
+
+        UtilCadena util = new UtilCadena();
+
+        String resultado = util.eliminarUltimaPalabra(frase);
+
+        System.out.println("Frase resultante: " + resultado);
+
+        scanner.close();
+    }
 }
--- src/poo/pagina61/ejercicio4/UtilCadena.java
+++ src/poo/pagina61/ejercicio4/UtilCadena.java
@@ -1,4 +1,18 @@
 package poo.pagina61.ejercicio4;
 
 public class UtilCadena {
+
+    public String eliminarUltimaPalabra(String frase) {
+
+        frase = frase.trim();
+
+        int ultimaPosicion = frase.lastIndexOf(" ");
+
+        if (ultimaPosicion == -1) {
+
+            return "";
+        }
+
+        return frase.substring(0, ultimaPosicion);
+    }
 }

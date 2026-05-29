1c534dc986bce5723aa7253dd6264626cbbe5f56
Andrea <denissedepaz16@gmail.com>
2026-05-28 19:32:08 -0600

    Andrea Denisse Ortiz De Paz-Ejercicio2 modificado - pagina 61- finalizado

--- src/poo/pagina61/ejercicio2/Main.java
+++ src/poo/pagina61/ejercicio2/Main.java
@@ -6,13 +6,13 @@ import java.util. Scanner;
       public static void main(String[] args){
 
           Scanner sc = new Scanner(System.in);
-          System.out.println("Ingrese Texto");
+          System.out.println("Ingrese Texto:");
 
           String texto = sc.nextLine();
 
           UtilCadena util = new UtilCadena();
           String resultado = util.QuitarEspacios(texto);
-          
+
           System.out.println("Resultado:"+ resultado);
 
 
--- src/poo/pagina61/ejercicio2/UtilCadena.java
+++ src/poo/pagina61/ejercicio2/UtilCadena.java
@@ -1,9 +1,9 @@
 package poo.pagina61.ejercicio2;
 
 public class UtilCadena {
-     public String QuitarEspacios(String texto){
+     public String QuitarEspacios(String texto) {
 
-         return texto.replace("","");
+         return texto.replace(" ","");
 
      }
 }
